a81c42316758aba2058969d1318fed0f4cc1b476
Milena Leonardi <milena.leonardi@volvo.com>
2024-03-08 09:23:42 -0300

    Initial commit

--- obj/Debug/net6.0/DesafioEntrevistaTecnica.API.AssemblyInfo.cs
+++ obj/Debug/net6.0/DesafioEntrevistaTecnica.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioEntrevistaTecnica.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435615480404364af54973dd9f5d9844f45b867c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioEntrevistaTecnica.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioEntrevistaTecnica.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
