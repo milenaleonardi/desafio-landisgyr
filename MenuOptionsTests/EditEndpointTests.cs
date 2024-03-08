d7c88b3fbbbaac3f931bc8b4df09bc57373ae545
Milena Vargas Leonardi <milena.leonardi@hotmail.com>
2024-03-08 11:21:04 -0300

    Update EditEndpointTests.cs

--- MenuOptionsTests/EditEndpointTests.cs
+++ MenuOptionsTests/EditEndpointTests.cs
@@ -1,4 +1,4 @@
-﻿using DesafioEntrevistaTecnica.API.Models;
+using DesafioEntrevistaTecnica.API.Models;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
@@ -14,66 +14,43 @@ namespace DesafioEntrevistaTecnica.API.MenuOptions
         public void EditEndpoint_ExistingEndpoint_ShouldEditSwitchState()
         {
             // Arrange
-            string endpointSerialNumber = "ABC123";
             List<EnergyCompany> endpoints = new List<EnergyCompany>
             {
-                new EnergyCompany(endpointSerialNumber, "NSX1P2W", 12345, "1.0", "Connected")
+                new EnergyCompany("ABC123", "NSX1P2W", 12345, "1.0", "Connected")
             };
 
             // Mock user input/output
+            var consoleInput = new StringReader("ABC123\nDisconnected\n");
+            Console.SetIn(consoleInput);
 
-            // Provide Endpoint Serial Number 
-            Console.SetIn(new StringReader(endpointSerialNumber));
             var consoleOutput = new StringWriter();
             Console.SetOut(consoleOutput);
-
-            // Provide new value for State
-            Console.SetIn(new StringReader("Disconnected"));
-            var consoleNewValueOutput = new StringWriter();
-            Console.SetOut(consoleNewValueOutput);
 
             // Act
             EditEnergyCompanyEndpoint.EditEndpoint(endpoints);
 
             // Assert
-            StringAssert.Contains($"Endpoint Serial Number: {endpointSerialNumber}", consoleOutput.ToString());
-
+            var editedEndpoint = endpoints.Find(endpoint => endpoint.EndpointSerialNumber == "ABC123");
+            Assert.AreEqual(1, editedEndpoint.SwitchState);
+            StringAssert.Contains($"\nEndpoint was edited to Disconnected with success.", consoleOutput.ToString());
         }
+
 
         [Test]
         public void EditEndpoint_NonExistingEndpoint_ShouldThrowException()
         {
             // Arrange
-            string endpointSerialNumber = "XYZ456";
             List<EnergyCompany> endpoints = new List<EnergyCompany>
             {
                 new EnergyCompany("ABC123", "NSX1P2W", 12345, "1.0", "Connected")
             };
 
             // Mock user input/output
-            Console.SetIn(new StringReader(endpointSerialNumber));
-            Console.SetIn(new StringReader("Disconnected"));
+            Console.SetIn(new StringReader("XYZ456\nDisconnected\n"));
 
             // Act & Assert
             Assert.Throws<Exception>(() => EditEnergyCompanyEndpoint.EditEndpoint(endpoints));
         }
 
-        [Test]
-        public void EditEndpoint_ExistingEndpoint_StateValueException()
-        {
-            // Arrange
-            string endpointSerialNumber = "ABC123";
-            List<EnergyCompany> endpoints = new List<EnergyCompany>
-            {
-                new EnergyCompany("ABC123", "NSX1P2W", 12345, "1.0", "Connected")
-            };
-
-            // Mock user input/output
-            Console.SetIn(new StringReader(endpointSerialNumber));
-            Console.SetIn(new StringReader("Other value"));
-
-            // Act & Assert
-            Assert.Throws<Exception>(() => EditEnergyCompanyEndpoint.EditEndpoint(endpoints));
-        }
     }
 }
